99acd01be9c74edd053d4416393ab16585ddb1a5
TheTrabin <beyondtears@gmail.com>
2020-05-30 06:01:46 -0700

    Added Home Screen, Loading Screen, and junctioned a lot into various other scenes. Also, total game over status now. Routes to main menu.

---
+++ js/scenes/homeScene.js
@@ -0,0 +1,37 @@
+// create a new scene
+let homeScene = new Phaser.Scene('Home');
+
+homeScene.create = function() {
+
+//game background, with active input
+let bg = this.add.sprite( 0, 0, 'backyard').setOrigin(0,0).setInteractive();
+
+
+
+//welcome text
+let gameW = this.sys.game.config.width;
+let gameH = this.sys.game.config.height;
+
+let text = this.add.text(gameW/2, gameH/2, '😁 VIRTUAL PET', {
+    font: '40px Ariel',
+    fill: '#ffffff',
+
+});
+text.setOrigin(0.5);
+text.depth = 1;
+
+//text background
+let textBg = this.add.graphics();
+textBg.fillStyle(0x000000, 0.7);
+textBg.fillRect(
+    gameW/2 - text.width/2 -10,
+    gameH/2 - text.height/2 -10,
+    text.width +20,
+    text.height +20,
+);
+
+bg.on('pointerdown', function(){
+    this.scene.start('Game');
+
+}, this);
+};
