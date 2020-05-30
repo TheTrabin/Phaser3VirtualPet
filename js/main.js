f0481c2f93fe614eea23146809ae4377e503cb5b
TheTrabin <beyondtears@gmail.com>
2020-05-30 01:29:43 -0700

    UI, Basic Spriting, and setup

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@
 let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
-gameScene.init = function() {};
+gameScene.init = function() {
+  // game stats
+  this.stats = {
+    health: 100,
+    fun: 100,
+
+  };
+
+
+};
 
 // load asset files for our game
 gameScene.preload = function() {
@@ -13,10 +22,40 @@ gameScene.preload = function() {
   this.load.image('candy', 'assets/images/candy.png');
   this.load.image('rotate', 'assets/images/rotate.png');
   this.load.image('toy', 'assets/images/rubber_duck.png');
+
+  this.load.spritesheet("pet", 'assets/images/pet.png', {
+    frameWidth: 97,
+    frameHeight: 83,
+    margin: 1, 
+    spacing: 1,
+});
 };
 
 // executed once, after assets were loaded
-gameScene.create = function() {};
+gameScene.create = function() {
+
+  //game Background
+  this.bg = this.add.sprite( 0, 0, 'backyard').setOrigin(0,0);
+
+  this.pet = this.add.sprite(100,200, 'pet', 0);
+
+//create ui
+this.createUi();
+
+
+};
+
+//create ui
+gameScene.createUi = function() {
+  //buttons
+  this.appleBtn = this.add.sprite(72,570, 'apple').setInteractive();
+  this.candyBtn = this.add.sprite(144,570, 'candy').setInteractive();
+  this.toyBtn = this.add.sprite(216,570, 'toy').setInteractive();
+  this.rotateBtn = this.add.sprite(288,570, 'rotate').setInteractive();
+
+
+
+};
 
 // our game's configuration
 let config = {
